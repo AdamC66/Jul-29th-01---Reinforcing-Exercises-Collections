603cba1956e1cd961d553911b579634f66c3fa42
adamc66 <adam.cote66@gmail.com>
2019-07-29 13:59:38 -0400

    completed reinforcement exercise

--- reinforcement.py
+++ reinforcement.py
@@ -13,6 +13,6 @@ fr = ['un','deux','trois','quatre','cinq','six','sept','huit','neuf']
 
 transform = {}
 for i in range(len(digits)):
-    transform[i+1] = {'French': fr[i], 'English':en[i]}
+    transform[str(i+1)] = {'French': fr[i], 'English':en[i]}
 
 print (transform)
